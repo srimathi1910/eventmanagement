d4e74b80ba1a319102058e2051b112cd34357f93
srimathi1910 <srimathin0@gmail.com>
2024-06-01 23:05:17 +0530

    push

---
+++ footer.php
@@ -0,0 +1,20 @@
+  <!-- Footer
+    ================================================== -->
+    <footer class="footer">
+      <div class="container">
+      <center>
+      
+      <font size="4">Judging Management System &COPY; <?= date('Y') ?> </font>
+      <hr />
+      
+      
+   
+      
+      
+      
+      </center>
+      
+      </div>
+    </footer>
+
+   
